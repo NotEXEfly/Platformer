59f8159ada7a58c2e4b5668387dadec69e9b4f74
NotEXE <alekseipyanov@mail.ru>
2021-02-09 22:48:08 +0300

    add player animations

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _traget;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(_traget.position.x, _traget.position.y, transform.position.z);
+    }
+}
